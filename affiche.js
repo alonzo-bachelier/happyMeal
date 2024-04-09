fd4cd87434ce6ff0cd9129493b2814a22c088249
aaliyah-houidef <aaliyah.houidef@laplateforme.io>
2024-04-09 16:26:48 +0200

    ajout de fichier html plus ajout de fonction pour afficher certains nombres de recettes par pages

--- affiche.js
+++ affiche.js
@@ -1,4 +1,20 @@
-function afficherRecettes() {
+let recette = [
+  "Poulet rôti aux herbes",
+  "Salade de quinoa aux légumes grillés",
+  "Tarte aux pommes",
+  "Soupe de lentilles",
+  "Pâtes Carbonara",
+  "Salade de fruits frais",
+  "Ratatouille provençale",
+  "Salade César",
+  "Risotto aux champignons",
+  "Muffins aux myrtilles",
+  "Lasagnes végétariennes",
+  "Salade niçoise",
+  "Tiramisu",
+];
+
+function afficherRecettes(page) {
   fetch("../json/data.json")
     .then((response) => response.json())
     .then((data) => {
@@ -45,3 +61,43 @@ function afficherRecettes() {
 }
 
 document.addEventListener("DOMContentLoaded", afficherRecettes);
+
+function afficherRecettes() {}
+const startIdx = (page - 1) * 5;
+const endIdx = startIdx + 5;
+
+const sectionRecettes = document.querySelector(".repas");
+sectionRecettes.innerHTML = ""; // Clear la section des recettes
+for (let index = startIdx; index < endIdx && index < recette.length; index++)
+  function changerPage(page) {
+    currentPage = page;
+    afficherRecettes(currentPage);
+  }
+
+document.getElementById("precedent").addEventListener("click", () => {
+  if (currentPage > 1) {
+    changerPage(currentPage - 1);
+  }
+});
+
+document.getElementById("suivant").addEventListener("click", () => {
+  if (currentPage < 3) {
+    changerPage(currentPage + 1);
+  }
+});
+
+document.getElementById("1").addEventListener("click", () => {
+  changerPage(1);
+});
+
+document.getElementById("2").addEventListener("click", () => {
+  changerPage(2);
+});
+
+document.getElementById("3").addEventListener("click", () => {
+  changerPage(3);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  afficherRecettes(currentPage);
+});
