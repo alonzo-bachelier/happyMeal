f9a5aaa865404227b573d887d835a4d8091fee40
alonzo-bachelier <alonzo.bachelier@laplateforme.io>
2024-04-09 10:07:40 +0200

    push avant checkout

--- javascript/accueil.js
+++ javascript/accueil.js
@@ -1,4 +1,4 @@
-$(document).ready(() => {
+function recharger() {
     fetch("../json/data.json")
         .then(response => response.json())
         .then(data => {
@@ -17,38 +17,51 @@ $(document).ready(() => {
                 index => data.recettes[index]
             );
 
-            // Mise à jour de la première recette
             $(".nom-aleatoire-un").text(recettesAleatoires[0].nom);
             $("#ingredients-un").empty();
             recettesAleatoires[0].ingredients.forEach(ingredient => {
-                $("#ingredients-un").append(
-                    `<li>${ingredient.quantite} de ${ingredient.nom}</li>`
-                );
+                if (typeof ingredient === "string") {
+                    $("#ingredients-un").append(`<li>${ingredient}</li>`);
+                } else {
+                    $("#ingredients-un").append(
+                        `<li>${ingredient.quantite} de ${ingredient.nom}</li>`
+                    );
+                }
             });
             $("#img-recette1").html(
                 `<img src="${recettesAleatoires[0].images}" class="activator img-recettes" style="width: 100%;">`
             );
 
-            // Mise à jour de la deuxième recette
             $(".nom-aleatoire-deux").text(recettesAleatoires[1].nom);
             $("#ingredients-deux").empty();
             recettesAleatoires[1].ingredients.forEach(ingredient => {
-                $("#ingredients-deux").append(
-                    `<li>${ingredient.quantite} de ${ingredient.nom}</li>`
-                );
+                if (typeof ingredient === "string") {
+                    $("#ingredients-deux").append(`<li>${ingredient}</li>`);
+                } else {
+                    $("#ingredients-deux").append(
+                        `<li>${ingredient.quantite} de ${ingredient.nom}</li>`
+                    );
+                }
             });
             $("#img-recette2").html(
                 `<img src="${recettesAleatoires[1].images}" class="activator img-recettes" style="width: 100%;">`
             );
 
-            // Mise à jour de la troisième recette
+            // Correction supposée
             $(".nom-aleatoire-trois").text(recettesAleatoires[2].nom);
             $("#ingredients-trois").empty();
             recettesAleatoires[2].ingredients.forEach(ingredient => {
-                $("#ingredients-trois").append(
-                    `<li>${ingredient.quantite} de ${ingredient.nom}</li>`
-                );
+                // Vérifie si l'ingrédient est une chaîne pour la 'Salade de fruits frais'
+                if (typeof ingredient === "string") {
+                    $("#ingredients-trois").append(`<li>${ingredient}</li>`);
+                } else {
+                    // Sinon, l'ingrédient est traité comme un objet
+                    $("#ingredients-trois").append(
+                        `<li>${ingredient.quantite} de ${ingredient.nom}</li>`
+                    );
+                }
             });
+
             $("#img-recette3").html(
                 `<img src="${recettesAleatoires[2].images}" class="activator img-recettes" style="width: 100%;">`
             );
@@ -56,4 +69,19 @@ $(document).ready(() => {
         .catch(error =>
             console.error("Erreur lors du chargement des données JSON:", error)
         );
+    $("#add-bouton1").click(() => {
+        recettesAleatoires[0];
+    });
+}
+recharger();
+$("#recharger").on("click", recharger);
+
+$("#add-bouton1").on("click", () => {
+    $(this).text("delete_forever");
+});
+$("#add-bouton2").on("click", () => {
+    $(this).text("delete_forever");
+});
+$("#add-bouton3").on("click", () => {
+    $(this).text("delete_forever");
 });
