5f3ba770a0c50a0bd8e04496ebc7f64b3b276453
aaliyah-houidef <aaliyah.houidef@laplateforme.io>
2024-04-12 15:15:24 +0200

    ajout modal a l'html

--- calendrier.js
+++ calendrier.js
@@ -1,3 +1,8 @@
+// 1. Faire le merge avec Alonzo pour récupérer les favoris
+// 2. Avec les favoris et sur ta page du calendrier, configurer fullCalendar pour afficher la modal lors du clique sur une date,
+// et afficher la liste des favoris pour ajouter sur le calendrier. Il faut ensuite ajouter la date du calendrier + le nom de la
+// recette pour garder les évcénements actifs. (C.F: Alexandre à réussi)
+
 document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("calendar");
 
@@ -20,16 +25,12 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   calendar.render();
+  calendarEl.addEventListener("dateClick", function (info) {
+    var modal = document.getElementById("exampleModal");
+    modal.style.display = "block";
+    // Ici, vous pouvez traiter la date cliquée et ajouter le nom de la recette au calendrier
+    // Par exemple, vous pouvez utiliser info.dateStr pour obtenir la date cliquée au format "YYYY-MM-DD"
+    // Et afficher la date dans la modal pour que l'utilisateur puisse la sélectionner
+    console.log("Date cliquée : " + info.dateStr);
+  });
 });
-
-const listefav = document.getElementById("listefav");
-const listeFavoris = localStorage.getItem("favoris");
-JSON.parse(listeFavoris).forEach((fav) => {
-  console.log(fav);
-  const p = document.createElement("p");
-  p.innerHTML = `${fav} <button id="supprFav">x</button> `;
-
-  listefav.append(p);
-});
-
-console.log(listeFavoris);
